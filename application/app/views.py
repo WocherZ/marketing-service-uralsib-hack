9b3965b573e4d863239431e8c000fa163c0ed06a
Vladimir <cay108@yandex.ru>
2023-06-24 19:19:48 +0300

    edit pages & urls

--- application/app/views.py
+++ application/app/views.py
@@ -2,19 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.http import HttpResponse
 
-
-class HomeView(View):
-    def get(self, request):
-        context = {}
-
-        return render(request, 'base_template.html', context)
+def home(request):
+    return render(request, 'home.html')
 
 def register(request):
     return render(request, 'register.html')
 
 def authorization(request):
     return render(request, 'authorization.html')
-
-def main_page(request):
-    return render(request, 'base_template.html')
-
