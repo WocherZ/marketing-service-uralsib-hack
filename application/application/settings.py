b71ee4023839eafad818d5f3529d451f844899cb
MirAn55500 <andrey_mironov02@mail.ru>
2023-06-30 00:04:54 +0300

    Добавил хрень какую-то, логику не понимаю пока, но оно работает. Дайте блин нормальное ТЗ

--- application/application/settings.py
+++ application/application/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'postgres', # Имя базы данных
         'USER': 'postgres', # Имя пользователя
         'PASSWORD': 'postgres', # Пароль пользователя
-        'HOST': 'pgdb', # Наименование контейнера для базы данных в Docker Compose
+        'HOST': '', # Наименование контейнера для базы данных в Docker Compose
         'PORT': '5432',  # Порт базы данных
     }
 }
